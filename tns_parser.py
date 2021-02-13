296dd3a0c1f677a5d153079c6509500c0a8b6fc0
0xCoto <cotothearcher@gmail.com>
2021-02-13 16:57:32 +0200

    Add SEFD and S:N columns

--- tns_parser.py
+++ tns_parser.py
@@ -15,17 +15,19 @@ new_f = new_f[['Name', 'Discovery Date (UT)', 'RA', 'DEC', 'DM', 'Discovery Mag/
 new_f.columns = ['frb', 'utc', 'ra', 'dec', 'dm', 'flux']
 
 # Strip whitespace from FRB names
-new_f["frb"] = new_f["frb"].str.split().apply("".join)
+new_f['frb'] = new_f['frb'].str.split().apply(''.join)
 
 # Insert columns
-new_f.insert(2, 'mjd', '0')
-new_f.insert(3, 'telescope', '0')
-new_f.insert(6, 'l', '0')
-new_f.insert(7, 'b', '0')
-new_f.insert(8, 'frequency', '0')
-new_f.insert(11, 'width', '0')
-new_f.insert(12, 'fluence', '0')
-new_f.insert(13, 'status', 'N')
+new_f.insert(2, 'mjd', '?')
+new_f.insert(3, 'telescope', '?')
+new_f.insert(4, 'sefd', '?')
+new_f.insert(7, 'l', '?')
+new_f.insert(8, 'b', '?')
+new_f.insert(9, 'frequency', '?')
+new_f.insert(12, 'width', '?')
+new_f.insert(13, 'fluence', '?')
+new_f.insert(14, 'snr', '?')
+new_f.insert(15, 'status', 'N')
 
 # Write changes to file
 new_f.to_csv('catalogue.csv', index=False, quoting=csv.QUOTE_NONNUMERIC)
