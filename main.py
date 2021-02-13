4782e87ba041e428e18dde5adedb60064018d751
0xCoto <cotothearcher@gmail.com>
2021-02-13 15:36:57 +0200

    Clean code

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from pymongo import MongoClient
 import csv
 import json
-#Mongo Connection
+
+# Initiate Mongo connection
 client = MongoClient("mongodb://localhost:27017/")
 db = client["frbstats"]
 collection = db["catalogue"]
-#drop all data 
+
+# Drop all data
 collection.drop()
 
+# Load catalogue data
 with open('catalogue.json', 'r') as myfile:
-    data=myfile.read()
+    data = myfile.read()
     obj = json.loads(data)
     for obs in obj:
         collection.insert_one(obs)
