744845ac3a60d4026c7c9bf07d4bad827d2e2372
0xCoto <cotothearcher@gmail.com>
2021-02-15 22:28:34 +0200

    Change index to root directory

--- catalogue.php
+++ catalogue.php
@@ -34,7 +34,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href=".">
                 <div class="sidebar-brand-icon rotate-n-0">
                     <i class="fas fa-database"></i>
                 </div>
