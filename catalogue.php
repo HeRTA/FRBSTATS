d737eda99db3f1265857f7b81f9e9e58bddbbc43
0xCoto <cotothearcher@gmail.com>
2021-03-13 23:57:50 +0200

    Update catalogue.php

--- catalogue.php
+++ catalogue.php
@@ -290,13 +290,13 @@
     } );
     </script>
     <script>
-    $(document).ready(function()
+    /*$(document).ready(function()
     {
         // Alternate row colors
         $("tr:even").css("background-color", "#F2F2F2");
         $("tr:odd").css("background-color", "#FFFFFF");
         $("tr:eq(0)").css("background-color", "#D9D9D9");
-    });
+    });*/
     function colorize() {
         // Alternate row colors
         $("tr:even").css("background-color", "#F2F2F2");
