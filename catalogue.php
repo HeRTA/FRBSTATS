5a573037a562fdb66fc649aa0d882b180acf56e8
0xCoto <cotothearcher@gmail.com>
2021-03-14 01:22:45 +0200

    Change onclick to onmouseover

--- catalogue.php
+++ catalogue.php
@@ -151,7 +151,7 @@
                           <a style="font-weight: bold" id="verified" onclick="if (document.getElementById(this.id).style.fontWeight == 'normal' || document.getElementById(this.id).style.fontWeight == '') {document.getElementById(this.id).style.fontWeight = 'bold'}else {document.getElementById(this.id).style.fontWeight = 'normal';}" class="toggle-vis" data-column="15">Status</a>
                         </div>
                             <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0" onload="colorize()" onclick="colorize()">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0" onload="colorize()" onmouseover="colorize()">
                                     <thead>
                                         <tr>
                                             <th>FRB</th>
