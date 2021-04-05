1ebf38ee13227eae9673074897467cce5876621e
0xCoto <cotothearcher@gmail.com>
2021-04-05 03:21:06 +0300

    Lowercase scripts

--- catalogue.php
+++ catalogue.php
@@ -21,7 +21,7 @@
     <link href="css/style.min.css" rel="stylesheet">
 
     <!-- Custom styles for page -->
-    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="vendor/datatables/datatables.bootstrap4.min.css" rel="stylesheet">
 
 </head>
 
@@ -260,8 +260,8 @@
     <script src="js/style.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="vendor/datatables/jquery.datatables.min.js"></script>
+    <script src="vendor/datatables/datatables.bootstrap4.min.js"></script>
 
     <!-- Page level custom scripts -->
     <script src="js/demo/datatables-demo.js"></script>
