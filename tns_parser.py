8be458d26b593c4cd8e968b23b7998ad6f635996
0xCoto <cotothearcher@gmail.com>
2021-04-06 22:55:43 +0300

    Fill ref column

--- tns_parser.py
+++ tns_parser.py
@@ -20,14 +20,13 @@ new_f.columns = ['frb', 'utc', 'ra', 'dec', 'dm', 'flux']
 # Insert columns
 new_f.insert(2, 'mjd', 'x')
 new_f.insert(3, 'telescope', 'x')
-new_f.insert(4, 'sefd', 'x')
-new_f.insert(7, 'l', 'x')
-new_f.insert(8, 'b', 'x')
-new_f.insert(9, 'frequency', 'x')
-new_f.insert(12, 'width', 'x')
-new_f.insert(13, 'fluence', 'x')
-new_f.insert(14, 'snr', 'x')
-new_f.insert(15, 'status', 'N')
+new_f.insert(6, 'l', 'x')
+new_f.insert(7, 'b', 'x')
+new_f.insert(8, 'frequency', 'x')
+new_f.insert(11, 'width', 'x')
+new_f.insert(12, 'fluence', 'x')
+new_f.insert(13, 'snr', 'x')
+new_f.insert(14, 'ref', 'N')
 
 # Write changes to file
 new_f.to_csv('catalogue.csv', index=False, quoting=csv.QUOTE_NONNUMERIC)
