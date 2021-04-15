51fe1842c2065c925c1773d15d449bca3b3cb195
Apostolos <cotothearcher@gmail.com>
2021-04-15 07:17:46 +0300

    Delete axis limits and remove cmap

--- figs/fluence_dm.py
+++ figs/fluence_dm.py
@@ -66,20 +66,20 @@ for idx in sorted(idx_mask, reverse=True):
 plt.grid(color='grey', linestyle='-', linewidth=0.25, alpha=1)
 
 # Scatter plot
-plt.scatter(dm, fluence, c=dm, s=500, alpha=0.7, edgecolor='black', linewidth=2, cmap='plasma', zorder=10)
+plt.scatter(dm, fluence, s=500, alpha=0.7, edgecolor='black', linewidth=2, cmap='plasma', zorder=10)
 
 # Set axis labels & figure title
 plt.xlabel(r'$\mathrm{Dispersion \ Measure \ }\Bigg[\mathrm{pc \ cm}^{-3}\Bigg]$', fontsize=52)
-plt.ylabel(r'$\mathrm{Burst Fluence \ [Jy ms]}$', fontsize=52)
+plt.ylabel(r'$\mathrm{Burst \ Fluence \ [Jy \ ms]}$', fontsize=52)
 plt.title(r'$\mathrm{FRB \ Fluence-DM \ Distribution}$', fontsize=72, y=1.01)
 
 # Set log-log scaling
 plt.xscale('log')
 plt.yscale('log')
 
-# Set ylim
-plt.xlim(10**-1,10**4)
-plt.ylim(10**-2,10**3)
+# Set axis limits
+#plt.xlim(10**-1,10**4)
+#plt.ylim(10**-2,10**3)
 
 # Set tick size
 plt.xticks(fontsize=42, y=-0.005)
