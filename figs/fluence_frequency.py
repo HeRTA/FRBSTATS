f6621444f6707d46a75494b055c60fb4a64a989c
Apostolos <cotothearcher@gmail.com>
2021-04-19 02:22:15 +0300

    Add PDF

--- figs/fluence_frequency.py
+++ figs/fluence_frequency.py
@@ -96,4 +96,5 @@ plt.tight_layout()
 
 # Save data to a scalable format
 plt.savefig('fluence_frequency.svg', format='svg')
+plt.savefig('fluence_frequency.pdf')
 plt.savefig('fluence_frequency.png')
