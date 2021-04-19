dd6e347740a2dc6f7177f146cc3ea67ac059f414
Apostolos <cotothearcher@gmail.com>
2021-04-19 12:27:21 +0300

    Delete trailing comma

--- api/search.php
+++ api/search.php
@@ -3,26 +3,28 @@
 $file = '../catalogue.json';
 $searchfor = '20210401A';
 
-// the following line prevents the browser from parsing this as HTML.
+// Prevent HTML parsing
 header('Content-Type: text/plain');
 
-// get the file contents, assuming the file to be readable (and exist)
-$contents = file_get_contents($file);
+// Grab file conents
+$content = file_get_contents($file);
 
-// escape special characters in the query
+// Escape special chars
 $pattern = preg_quote($searchfor, '/');
 
-// finalise the regular expression, matching the whole line
-$pattern = "/^.*$pattern.*\$/m";
+// Finalize refex, matching line
+$regexPattern = "/^.*$pattern.*\$/m";
 
-// search, and store all matching occurences in $matches
-if (preg_match_all($pattern, $contents, $matches)) {
-  $entry = implode('\n', $matches[0]);
+// Search and store matching occurence in $match
+if (preg_match_all($regexPattern, $content, $match)) {
+  $entry = implode('\n', $match[0]);
+
+  // Delete trailing comma
+  $entry = substr($entry, 0, -1);
+  
+  // Output search result
   echo $entry;
-  $myObj = str_replace('\\', '', $entry);
 }
 
-$myJSON = json_encode($myObj);
-
-echo $myJSON;
+//$myJSON = json_encode($myObj);
 ?>
