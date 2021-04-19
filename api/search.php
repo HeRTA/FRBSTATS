3de479eae21c0d92f6be3277f40f8fc4796ca48c
Apostolos <cotothearcher@gmail.com>
2021-04-19 12:11:04 +0300

    Delete comment

--- api/search.php
+++ api/search.php
@@ -19,12 +19,8 @@ $pattern = "/^.*$pattern.*\$/m";
 if(preg_match_all($pattern, $contents, $matches)){
   $myObj = implode("\n", $matches[0]);
 }
-else{
-  echo "No matches found";
-}
 
 $myJSON = json_encode($myObj);
 
 echo $myJSON;
 ?>
-//{"l": "177.63", "snr": "15.58", "frb": "FRB 20210321A", "width": "-", "dec": "+26:11:24", "telescope": "CHIME", "utc": "2021-03-21 1:12:23", "ra": "5:07:50", "mjd": "59294.05027", "fluence": "-", "frequency": "600", "ref": "https://www.wis-tns.org/object/20210321a", "flux": "-", "dm": "412.46", "b": "-8.49"}
