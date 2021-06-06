c09cfcb88623c80a7e2ce1e69fa380730b3e9563
Apostolos <cotothearcher@gmail.com>
2021-06-06 22:30:44 +0300

    Fix index

--- repeater_parser.py
+++ repeater_parser.py
@@ -1,4 +1,4 @@
 repeaters_json = open('repeaters.json').read()
 lines = open('repeaters.html').read().splitlines()
-lines[162] = 'var treeData = '+str(repeaters_json)+';'
+lines[164] = 'var treeData = '+str(repeaters_json)+';'
 open('repeaters.html', 'w').write('\n'.join(lines))
