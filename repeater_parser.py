e33641fa684165d6c7b6d9903f8f8a49fc7e8097
Apostolos <cotothearcher@gmail.com>
2021-06-09 22:18:13 +0300

    Add empty line (code clearn)

--- repeater_parser.py
+++ repeater_parser.py
@@ -4,4 +4,5 @@ repeaters_json = open('repeaters.json').read()
 # Apply change to repeaters HTML page
 lines = open('repeaters.html').read().splitlines()
 lines[164] = 'var treeData = '+str(repeaters_json)+';'
+
 open('repeaters.html', 'w').write('\n'.join(lines))
