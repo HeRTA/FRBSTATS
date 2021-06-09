5b9685248354bd44d372264538d4057f3221097e
Apostolos <cotothearcher@gmail.com>
2021-06-09 20:19:23 +0300

    Remove form (href bug on mobile)

--- catalogue.php
+++ catalogue.php
@@ -109,11 +109,9 @@
                 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
                     <!-- Sidebar Toggle (Topbar) -->
-                    <form class="form-inline">
-                        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                            <i class="fa fa-bars"></i>
-                        </button>
-                    </form>
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
 
 
 
