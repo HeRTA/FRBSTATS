edce85168962bc29da988566ffd30844762fa84c
Apostolos <cotothearcher@gmail.com>
2021-06-09 22:13:43 +0300

    Document code

--- repeater_parser.py
+++ repeater_parser.py
@@ -1,4 +1,7 @@
+# Load repeaters JSON file
 repeaters_json = open('repeaters.json').read()
+
+# Apply change to repeaters HTML page
 lines = open('repeaters.html').read().splitlines()
 lines[164] = 'var treeData = '+str(repeaters_json)+';'
 open('repeaters.html', 'w').write('\n'.join(lines))
