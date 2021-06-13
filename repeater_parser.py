22330668cc8b1f95d43a410e17e531a325c0a18c
Apostolos <cotothearcher@gmail.com>
2021-06-13 03:41:20 +0300

    Absolute path fix

--- repeater_parser.py
+++ repeater_parser.py
@@ -1,8 +1,8 @@
 # Load repeaters JSON file
-repeaters_json = open('~/public_html/frbstats/repeaters.json').read()
+repeaters_json = open('/home/herta-experiment/public_html/frbstats/repeaters.json').read()
 
 # Apply change to repeaters HTML page
-lines = open('~/public_html/frbstatsrepeaters.html').read().splitlines()
+lines = open('/home/herta-experiment/public_html/frbstats/repeaters.html').read().splitlines()
 lines[164] = 'var treeData = '+str(repeaters_json)+';'
 
-open('~/public_html/frbstatsrepeaters.html', 'w').write('\n'.join(lines))
+open('/home/herta-experiment/public_html/frbstats/repeaters.html', 'w').write('\n'.join(lines))
