cb0564f2fd05a25d4e475eac69518b37eaf68f7f
Apostolos <cotothearcher@gmail.com>
2021-06-13 03:34:56 +0300

    Absolute path

--- repeater_parser.py
+++ repeater_parser.py
@@ -1,8 +1,8 @@
 # Load repeaters JSON file
-repeaters_json = open('repeaters.json').read()
+repeaters_json = open('~/public_html/frbstats/repeaters.json').read()
 
 # Apply change to repeaters HTML page
-lines = open('repeaters.html').read().splitlines()
+lines = open('~/public_html/frbstatsrepeaters.html').read().splitlines()
 lines[164] = 'var treeData = '+str(repeaters_json)+';'
 
-open('repeaters.html', 'w').write('\n'.join(lines))
+open('~/public_html/frbstatsrepeaters.html', 'w').write('\n'.join(lines))
