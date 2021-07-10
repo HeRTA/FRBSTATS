2f0579f0ef1e0d307c9c6f5046c5d8f887577311
Apostolos <cotothearcher@gmail.com>
2021-07-10 03:15:08 +0300

    Fix blanks

--- fetch_catalogue.py
+++ fetch_catalogue.py
@@ -1,5 +1,15 @@
 import urllib.request
+import os
 
 # Fetch sheet as CSV
 sheet_url = 'https://docs.google.com/spreadsheets/d/1W27KNa6yJzYA_b8HLSz4hxtWEZQtxUhGTXfQjlXgpzY/gviz/tq?tqx=out:csv&sheet={{Catalogue}}'
-urllib.request.urlretrieve(sheet_url, 'catalogue.csv')
+urllib.request.urlretrieve(sheet_url, 'catalogue_tmp.csv')
+
+# Replace blank instances with "-"
+with open('catalogue_tmp.csv', 'rt') as fin:
+	with open('catalogue.csv', 'wt') as fout:
+		for line in fin:
+			fout.write(line.replace('""', '"-"'))
+
+# Delete temporary catalogue file
+os.remove('catalogue_tmp.csv')
